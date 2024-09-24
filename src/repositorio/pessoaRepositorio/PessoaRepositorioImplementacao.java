16aa85edaf86bf2c590a32a1ffac633275b61a7d
Allana <allanac.avila@gmail.com>
2024-09-24 18:30:55 -0300

    alterando modelo de busca de pessoa

--- src/repositorio/pessoaRepositorio/PessoaRepositorioImplementacao.java
+++ src/repositorio/pessoaRepositorio/PessoaRepositorioImplementacao.java
@@ -3,35 +3,44 @@ package repositorio.pessoaRepositorio;
 import exception.pessoaException.PessoaDuplicadaException;
 import exception.pessoaException.PessoaNaoEncontradaException;
 import modelo.pessoa.Pessoa;
+import modelo.pessoa.PessoaFisica;
+import modelo.pessoa.PessoaJuridica;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-public class PessoaRepositorioImplementacao <T extends Pessoa> extends PessoaRepositorio<T> {
-    public List<T> bancoDados;
+public class PessoaRepositorioImplementacao extends PessoaRepositorio<Pessoa> {
+    private List<Pessoa> bancoDados;
 
     public PessoaRepositorioImplementacao() {
-        this.bancoDados = new ArrayList<T>();
+        this.bancoDados = new ArrayList<>();
     }
 
     @Override
-    public T salvar(T pessoa) throws PessoaDuplicadaException {
-        if (bancoDados.stream().anyMatch(p -> p.getNomePessoa().equalsIgnoreCase(pessoa.getNomePessoa()))) {
-            throw new PessoaDuplicadaException("Pessoa com nome " + pessoa.getNomePessoa() + " já cadastrada.");
+    public Pessoa salvar(Pessoa pessoa) throws PessoaDuplicadaException, PessoaNaoEncontradaException {
+        if (pessoa instanceof PessoaFisica) {
+            if (buscarPorCpf(((PessoaFisica) pessoa).getCpf()).isPresent()) {
+                throw new PessoaDuplicadaException("Pessoa física com CPF " + ((PessoaFisica) pessoa).getCpf() + " já cadastrada.");
+            }
+        } else if (pessoa instanceof PessoaJuridica) {
+            if (buscarPorCnpj(((PessoaJuridica) pessoa).getCnpj()).isPresent()) {
+                throw new PessoaDuplicadaException("Pessoa jurídica com CNPJ " + ((PessoaJuridica) pessoa).getCnpj() + " já cadastrada.");
+            }
         }
+
         bancoDados.add(pessoa);
         return pessoa;
     }
 
     @Override
-    public List<T> listarPessoas() {
+    public List<Pessoa> listarPessoas() {
         return new ArrayList<>(bancoDados);
     }
 
     @Override
-    public T alterarPessoa(T pessoa) throws PessoaNaoEncontradaException {
-        Optional<T> optionalPessoa = buscarPorNome(pessoa.getNomePessoa());
+    public Pessoa alterarPessoa(Pessoa pessoa) throws PessoaNaoEncontradaException {
+        Optional<Pessoa> optionalPessoa = buscarPorIdentificador(pessoa instanceof PessoaFisica ? ((PessoaFisica) pessoa).getCpf() : ((PessoaJuridica) pessoa).getCnpj());
         if (optionalPessoa.isPresent()) {
             int index = bancoDados.indexOf(optionalPessoa.get());
             bancoDados.set(index, pessoa);
@@ -42,20 +51,37 @@ public class PessoaRepositorioImplementacao <T extends Pessoa> extends PessoaRep
     }
 
     @Override
-    public Optional<T> buscarPorNome(String nome) {
+    public Optional<Pessoa> buscarPorIdentificador(String identificador) throws PessoaNaoEncontradaException {
         return bancoDados.stream()
-                .filter(p -> p.getNomePessoa().equalsIgnoreCase(nome))
+                .filter(p -> (p instanceof PessoaFisica && ((PessoaFisica) p).getCpf().equals(identificador)) ||
+                        (p instanceof PessoaJuridica && ((PessoaJuridica) p).getCnpj().equals(identificador)))
                 .findFirst();
     }
 
     @Override
-    public T removerPessoa(String nome) throws PessoaNaoEncontradaException {
-        Optional<T> optionalPessoa = buscarPorNome(nome);
+    public Pessoa removerPessoa(String identificador) throws PessoaNaoEncontradaException {
+        Optional<Pessoa> optionalPessoa = buscarPorIdentificador(identificador);
         if (optionalPessoa.isPresent()) {
             bancoDados.remove(optionalPessoa.get());
             return optionalPessoa.get();
         } else {
             throw new PessoaNaoEncontradaException("Pessoa não encontrada.");
         }
+    }
+
+    @Override
+    public Optional<PessoaFisica> buscarPorCpf(String cpf) throws PessoaNaoEncontradaException {
+        return bancoDados.stream()
+                .filter(p -> p instanceof PessoaFisica && ((PessoaFisica) p).getCpf().equals(cpf))
+                .map(p -> (PessoaFisica) p)
+                .findFirst();
+    }
+
+    @Override
+    public Optional<PessoaJuridica> buscarPorCnpj(String cnpj) throws PessoaNaoEncontradaException {
+        return bancoDados.stream()
+                .filter(p -> p instanceof PessoaJuridica && ((PessoaJuridica) p).getCnpj().equals(cnpj))
+                .map(p -> (PessoaJuridica) p)
+                .findFirst();
     }
 }
